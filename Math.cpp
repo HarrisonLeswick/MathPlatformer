d1127e5cf43fe0819d2ba0392de758cddd317e6d
TheVoidAngel <50273250+TheVoidAngel@users.noreply.github.com>
2020-10-14 16:24:25 -0400

    Add files via upload

---
+++ Math.cpp
@@ -0,0 +1,21 @@
+//this is a test to see if the repo works
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main() {
+
+	int number;
+
+	cout << "give numba";
+	cin >> number;
+
+	cout << number;
+
+
+
+}
